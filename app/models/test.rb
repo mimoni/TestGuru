50855b6d0f6e0bf688692ce94901aad226c1eca1
Eugene Parshukov <ev.klit@gmail.com>
2020-01-26 20:54:44 +0300

    add task associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  has_many :questions
+  has_many :tests_users
+  has_many :users, through: :tests_users
+
   def self.titles_by_category_title(category_title)
     joins('JOIN categories ON tests.category_id = categories.id')
       .where('categories.title = ?', category_title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :tests_users
+  has_many :tests, through: :tests_users
+  has_many :created_tests, class_name: 'Test', foreign_key: 'user_id'
+
   def tests_by_level(level)
     Test.joins('JOIN tests_users ON tests.id = tests_users.test_id')
         .where('tests_users.user_id = ? and tests.level = ?', id, level)
