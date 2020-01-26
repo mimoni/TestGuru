d90104928dcc08228e09b5d3750c66cc8b311f5c
Eugene Parshukov <ev.klit@gmail.com>
2020-01-26 17:52:42 +0300

    fix argument where

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@
 class User < ApplicationRecord
   def tests_by_level(level)
     Test.joins('JOIN tests_users ON tests.id = tests_users.test_id')
-        .where(['tests_users.user_id = ? and tests.level = ?', id, level])
+        .where('tests_users.user_id = ? and tests.level = ?', id, level)
   end
 end
