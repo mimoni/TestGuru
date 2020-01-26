1799f97b871b8cc2dc99e8f24ca11d552b6b70bb
Eugene Parshukov <ev.klit@gmail.com>
2020-01-26 13:23:30 +0300

    rubocop autofix

--- db/migrate/20200126101852_create_join_table_user_test.rb
+++ db/migrate/20200126101852_create_join_table_user_test.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class CreateJoinTableUserTest < ActiveRecord::Migration[6.0]
   def change
     create_join_table :users, :tests do |t|
-       t.index :user_id
-       t.index :test_id
+      t.index :user_id
+      t.index :test_id
     end
   end
 end
