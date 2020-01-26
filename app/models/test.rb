ea1678e4c8f53529e61275e00030f5328577c64a
Eugene Parshukov <ev.klit@gmail.com>
2020-01-26 14:36:01 +0300

    fix names argument

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
-  def self.titles_by_category(_category_title)
+  def self.titles_by_category_title(category_title)
     joins('JOIN categories ON tests.category_id = categories.id')
-      .where(categories: { title: _category_title })
+      .where('categories.title = ?', category_title)
       .order(title: :desc)
       .pluck(:title)
   end
